b066fab73407404fa5df9ded50cfe5229afe4308
Maciej Dzik <dzikdrums@gmail.com>
2020-03-22 11:00:09 +0100

    add rating

--- server/models/product.model.js
+++ server/models/product.model.js
@@ -1,15 +1,16 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Product = new Schema({
-  id: { type: "String", required: true },
-  tag: { type: "String", required: false },
-  img: { type: "String", required: true },
-  name: { type: "String", required: true },
-  price: { type: "Number", required: true },
-  category: { type: "String", required: true },
-  qty: { type: "Number", required: true },
-  desc: { type: "String" }
+  id: { type: 'String', required: true },
+  tag: { type: 'String', required: false },
+  img: { type: 'String', required: true },
+  name: { type: 'String', required: true },
+  price: { type: 'Number', required: true },
+  category: { type: 'String', required: true },
+  qty: { type: 'Number', required: true },
+  rating: { type: 'Double', required: true },
+  desc: { type: 'String' }
 });
 
-module.exports = mongoose.model("Product", Product);
+module.exports = mongoose.model('Product', Product);
