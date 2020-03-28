6e4cd35f8ed4a2d62e57c9f7d955d94ad76840fb
Maciej Dzik <dzikdrums@gmail.com>
2020-03-28 11:47:23 +0100

    h

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -9,4 +9,10 @@ router.route('/products/category/:id').get(ProductController.getProduct);
 //get single product
 router.route('/product/:id').get(ProductController.getSingleProduct);
 
+//delete all products
+router.route('/products').delete(ProductController.deleteAllProducts);
+
+//add comment to product
+router.route('/product/:id').post(ProductController.addComment);
+
 module.exports = router;
